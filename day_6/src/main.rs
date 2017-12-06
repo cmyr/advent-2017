67b7213648404075cf6c6ab058f05ef95e19d9d2
Colin Rofls <colin@cmyr.net>
2017-12-06 11:36:02 -0500

    Very slightly less blah day 6

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -1,40 +1,62 @@
 use std::collections::HashSet;
 
 fn main() {
-    let input = "11	11	13	7	0	15	5	5	4	4	1	1	7	1	15	11"
+    let mut input = "11	11	13	7	0	15	5	5	4	4	1	1	7	1	15	11"
         .split_whitespace()
         .map(|s| usize::from_str_radix(s, 10).unwrap())
         .collect::<Vec<_>>();
 
-    let result = day_six(input.clone());
-    println!("{}", result.0);
-    println!("{}", result.1);
+    let result = part_one(&mut input);
+    println!("{}", result);
+
+    let part_two = count_loop_cycles(input);
+    println!("{}", part_two);
 
 }
 
+/// Part two. This code is.. :shrug:
+fn count_loop_cycles(inp: Vec<usize>) -> usize {
+    let mut loop_len = 0;
+    let initial_layout = inp.clone();
+    let mut current  = inp;
 
-fn day_six(mut inp: Vec<usize>) -> (usize, usize) {
+    loop {
+        current = next_layout(&current);
+
+        loop_len += 1;
+        if current == initial_layout { break }
+    }
+
+    loop_len
+}
+
+fn next_layout(inp: &[usize]) -> Vec<usize> {
+    let mut result = inp.to_owned();
+
+    let idx = result.iter().enumerate().fold((0, 0), |min, cur| {
+        if *cur.1 > min.1 {
+            (cur.0, *cur.1)
+        } else {
+            min
+        }
+    }).0;
+
+    let to_share = result[idx];
+    result[idx] = 0;
+    for i in 1..to_share + 1 {
+        let idx = (idx + i) % result.len();
+        result[idx] += 1;
+    }
+    result
+}
+
+fn part_one(inp: &mut [usize]) -> usize {
     let mut seen_states = HashSet::new();
     let mut num_ops = 0;
 
-    // part two
-    let mut in_loop = false;
-    let mut loop_len = 1;
-    let mut loop_start: Option<Vec<usize>> = None;
-
     loop {
-        if in_loop == true {
-            if loop_start.as_ref() == Some(&inp) {
-                return (num_ops - loop_len, loop_len)
-            } else {
-                loop_len += 1;
-            }
-        }
-        if !in_loop && !seen_states.insert(inp.clone()) {
-            in_loop = true;
-            loop_start = Some(inp.clone());
-        }
 
+        if !seen_states.insert(inp.to_owned()) { break }
         num_ops += 1;
 
         let idx = inp.iter().enumerate().fold((0, 0), |min, cur| {
@@ -52,6 +74,7 @@ fn day_six(mut inp: Vec<usize>) -> (usize, usize) {
             inp[idx] += 1;
         }
     }
+    num_ops
 }
 
 #[cfg(test)]
@@ -59,7 +82,15 @@ mod tests {
     use super::*;
     #[test]
     fn test_part_one() {
-        let inp = vec![0, 2, 7, 0];
-        assert_eq!(day_six(inp), (5, 4));
+        let mut inp = vec![0, 2, 7, 0];
+        assert_eq!(part_one(&mut inp), 5);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let mut inp = vec![0, 2, 7, 0];
+        part_one(&mut inp);
+        let num_cycles = count_loop_cycles(inp);
+        assert_eq!(num_cycles, 4);
     }
 }
